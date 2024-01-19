06282bdd889284fe982f76c06f823ae9d55680d8
Kaushal Raj <76280223+kaushal-create@users.noreply.github.com>
2024-01-19 22:48:01 +0530

    reduced emotions from 32 to 6

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -27,11 +27,7 @@ class Dataset(data.Dataset):
         """Reads source and target sequences from txt files."""
         self.vocab = vocab
         self.data = data 
-        self.emo_map = {
-        'surprised': 0, 'excited': 1, 'annoyed': 2, 'proud': 3, 'angry': 4, 'sad': 5, 'grateful': 6, 'lonely': 7,
-        'impressed': 8, 'afraid': 9, 'disgusted': 10, 'confident': 11, 'terrified': 12, 'hopeful': 13, 'anxious': 14, 'disappointed': 15,
-        'joyful': 16, 'prepared': 17, 'guilty': 18, 'furious': 19, 'nostalgic': 20, 'jealous': 21, 'anticipating': 22, 'embarrassed': 23,
-        'content': 24, 'devastated': 25, 'sentimental': 26, 'caring': 27, 'trusting': 28, 'ashamed': 29, 'apprehensive': 30, 'faithful': 31}
+        self.emo_map = { 'surprise': 0, 'joy': 1, 'anger': 2, 'sadness': 3, 'fear': 4, 'love': 5}
     def __len__(self):
         return len(self.data["target"])
 
